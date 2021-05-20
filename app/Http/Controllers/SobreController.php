f74ec8a1963eee27ceb041a69db60dd7916a7a70
anton <anton@DESKTOP-0RECEEQ.local>
2021-05-20 17:53:15 +0100

    Update SobreController.php and sobre.blade.php

--- app/Http/Controllers/SobreController.php
+++ app/Http/Controllers/SobreController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Marca;
+//use App\Models\Marca;
 
-class MarcasController extends Controller
+class SobreController extends Controller
 {
  public function index(){
      
--- resources/views/sobre.blade.php
+++ resources/views/sobre.blade.php
@@ -1,6 +1,7 @@
 @extends ('layout')
 
 @section ('titulo')
+
 @endsection
 
 @section ('navbar')
@@ -9,4 +10,8 @@
 @section ('menu')
 @endsection
 @section ('produtos')
+<h1>Sobre Nos</h1><br>
+<h3><p>Nós somos um Site de anuncio de peças de varios tipos para carros, motas etc<br>
+Site Criado por Manuel e Daniel</p></h3>
+
 @endsection
